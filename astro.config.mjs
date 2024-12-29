6a6619d21e66533073ea579695a959c4d4a1f89f
mwattier <mikedwattier@gmail.com>
2024-12-28 23:46:45 -0500

    Turn down the lights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://learnastro.stackwithmike.com"
+  site: "https://learnastro.stackwithmike.com",
+  integrations: [preact()]
 });
